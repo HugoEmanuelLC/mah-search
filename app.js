f965c728a0527e56ec4267f090963cd784286e0c
HugoEmanuelLC <helc85.info@gmail.com>
2023-12-04 11:34:58 +0100

    edit sign up

--- app.js
+++ app.js
@@ -1,14 +1,17 @@
 const express = require('express');
 const mongoose = require('mongoose');
+require('dotenv').config();
 const PORT = process.env.PORT || 3001;
 const dbURI = process.env.MONGODB_URI || process.env.DB_URI;
 const authRoutes = require('./routes/authRoutes');
 const cookieParser = require('cookie-parser');
 const { requireAuth, checkUser } = require('./middleware/authMiddleware');
 
+
 const app = express();
 
 // middleware
+// app.use(express.static('public'))
 app.use(express.json());
 app.use(cookieParser())
 
@@ -18,7 +21,6 @@ app.set("view engine", "ejs");
 app.use(express.static(__dirname + "/public"));
 
 // database connection
-
 mongoose.connect(dbURI)
   .then((result) => app.listen(PORT))
   .catch((err) => console.log(err));
