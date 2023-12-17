25ca82e523abd8d6a69966a19aa5a8bcf961011e
HugoEmanuelLC <helc85.info@gmail.com>
2023-12-17 21:30:39 +0100

    edit front projet hugo

--- routes/jobsRoutes.js
+++ routes/jobsRoutes.js
@@ -5,7 +5,7 @@ const jobsController = require('../controllers/jobsController')
 const router = Router();
 
 router.post('/create-job', jobsController.createJob)
-router.get('/create-job', (req, res) => res.render('createJob'))
+router.get('/create-job', (req, res) => {res.locals.user ? res.render('createJob') : res.render('login')})
 router.get('/list-jobs', jobsController.listJobs)
 router.get('/job/:id', jobsController.JobItem)
 router.put('/job/:id', jobsController.updateJob)
