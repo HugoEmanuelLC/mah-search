e130af2c01b2b5d832c0d980f69eaefdfa12d39c
HugoEmanuelLC <helc85.info@gmail.com>
2023-12-21 14:54:36 +0100

    filter

--- app.js
+++ app.js
@@ -17,6 +17,7 @@ const app = express();
 // middleware
 // app.use(express.static('public'));
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 
 // view engine
